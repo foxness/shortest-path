ec5b9db1ceec5bc399f05da9c511e963b589c6eb
foxneZz <foxnezz@gmail.com>
2017-11-03 22:15:23 +0300

    replaced the Node class with an array of neighbors

--- main.rb
+++ main.rb
@@ -9,28 +9,20 @@ a = # матрица смежности
 source = 0 # индекс начальной вершины
 destination = 5 # индекс конечной вершины
 
-# ^ все, что выше - входные данные программы ^
-
-class Node # класс вершины
-    attr_accessor :neighbors # вершина имеет только одно свойство - ее соседей
-
-    def initialize
-        @neighbors = []
-    end
-end
+# !! все, что выше - входные данные программы !!
 
 class Graph # класс графа
-    attr_accessor :nodes # граф имеет вершины
+    attr_accessor :nodes # граф имеет только вершины, которые представлены массивами их соседей
 
     # метод, создающий объект графа из данной матрицы смежности
     def initialize(matrix)
         n = matrix.length # n - количество вершин
         @nodes = []
-        n.times { @nodes << Node.new } # для начала заполняем граф n несвязанными вершинами
+        n.times { @nodes << [] } # для начала заполняем граф n несвязанными вершинами
         n.times do |y|
             n.times do |x|
                 if matrix[y][x] > 0
-                    @nodes[y].neighbors << @nodes[x] # связываем вершины, если элемент на пересечении матрицы больше нуля
+                    @nodes[y] << @nodes[x] # связываем вершины, если элемент на пересечении матрицы больше нуля
                 end
             end
         end
@@ -51,7 +43,7 @@ class Graph # класс графа
 
             unvisited.delete current # удаляем из непосещенных вершин текущую
 
-            current.neighbors.each do |neighbor| # для каждого соседа текущей вершины
+            current.each do |neighbor| # для каждого соседа текущей вершины
                 alt = distance[current] + 1 # предварительное расстояние до соседа = расстояние до текущей + 1
                 if alt < distance[neighbor] # если новое расстояние меньше старого
                     distance[neighbor] = alt # используем новое расстояние
@@ -74,23 +66,24 @@ class Graph # класс графа
     end
 end
 
-na = []
-n = (a.length ** 0.5).to_i # вычисляем сторону матрицы смежности
+n = (a.length ** 0.5).to_i # вычисляем сторону данной матрицы смежности
 
+# ниже преобразуем данную матрицу смежности из строки в двумерный массив чисел
+na = []
 n.times do |i|
-    na << a[i * n, n].chars.map { |s| s.to_i } # конвертируем матрицу из строки в двумерный массив чисел (одну матричную строку за итерацию)
-    p na.last # и в то же время выводим его
+    na << a[i * n, n].chars.map { |s| s.to_i } # преобразуем одну строку матрицы
+    p na.last # затем выводим ее
 end
 
-g = Graph.new na # создаем граф из данной матрицы смежности
+g = Graph.new na # создаем граф из преобразованной матрицы смежности
 result = g.find_shortest_path g.nodes[source], g.nodes[destination] # ищем кратчайший путь
 
-# делаем индексы начинающимися с 1 для удобного восприятия человеком
+# делаем данные индексы начинающимися с 1 для удобного восприятия человеком
 source += 1
 destination += 1
 
 if result[:found] # если путь найден
-    puts "The shortest path from #{source} to #{destination} has been found: #{result[:path]}}" # выводим его
+    puts "The shortest path from #{source} to #{destination} has been found: #{result[:path]}" # выводим его
 else
     puts "No path from #{source} to #{destination} has been found" # если не найден - то пишем, что пути нет
 end
